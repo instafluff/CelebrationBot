b8f30ad18fdf7b002a43a058b2552b24e05907b9
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-09-29 11:02:30 -0700

    Horrorscope

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 
 require('dotenv').config();
 
+var fetch = require( "node-fetch" );
 var Storage = require('node-storage');
 var ComfyJS = require("comfy.js");
 var store = new Storage('celebration.db');
 
 ComfyJS.onChat = () => {};
-ComfyJS.onCommand = ( user, command, message, flags ) => {
+ComfyJS.onCommand = async ( user, command, message, flags ) => {
   // !bday [month] [day] - saves the user's birthday
   // !bday - prints all the birthdays of the month / week
   // !bday [month] [day] [username] - saves the user's birthday
   // !bday [month] - print all the birthdays of the month
-  if( command == "bday" ) {
+  if( command === "bday" ) {
     if( message ) {
       // figure out month/day/username
       try {
@@ -64,6 +65,48 @@ ComfyJS.onCommand = ( user, command, message, flags ) => {
         var birthdayPeeps = getBirthdays( new Date().getMonth() );
         ComfyJS.Say( "This month's birthdays are: " + birthdayPeeps.map( x => `${x.name} (${monthToString(x.month)} ${x.day})` ).join( ", " ) );
       // }
+    }
+  }
+  else if( command === "horoscope" ) {
+    var userData = store.get( "birthdays" ) || {};
+    var username = user.toLowerCase();
+    if( message ) {
+      // Check it as a username
+      username = message.replace(/[^a-zA-Z0-9_]/g, "").toLowerCase();
+    }
+
+    if( userData[ username ] ) {
+      var birthday = userData[ username ];
+      console.log( birthday );
+      let zodiac = await fetch( `http://localhost:1111/zodiac?month=${birthday.month + 1}&date=${birthday.day}` ).then( r => r.json() );
+      if( zodiac.sign ) {
+        fetch( `http://localhost:1111/horoscope?fast=true&user=${username}&sign=${zodiac.sign}` );
+      }
+    }
+    else {
+      // Show username not found
+      ComfyJS.Say( `${username} was not found. Type !bday [month] [date] to add your birthday!` );
+    }
+  }
+  else if( command === "horrorscope" ) {
+    var userData = store.get( "birthdays" ) || {};
+    var username = user.toLowerCase();
+    if( message ) {
+      // Check it as a username
+      username = message.replace(/[^a-zA-Z0-9_]/g, "").toLowerCase();
+    }
+
+    if( userData[ username ] ) {
+      var birthday = userData[ username ];
+      console.log( birthday );
+      let zodiac = await fetch( `http://localhost:1111/zodiac?month=${birthday.month + 1}&date=${birthday.day}` ).then( r => r.json() );
+      if( zodiac.sign ) {
+        fetch( `http://localhost:1111/horrorscope?fast=true&user=${username}&sign=${zodiac.sign}` );
+      }
+    }
+    else {
+      // Show username not found
+      ComfyJS.Say( `${username} was not found. Type !bday [month] [date] to add your birthday!` );
     }
   }
 }
