1107d559e98a9934926d6d25f47bde8ee03025b7
instafluff <instaflufftv@gmail.com>
2021-04-09 11:47:05 -0700

    add Discord!

--- app.js
+++ app.js
@@ -112,6 +112,87 @@ ComfyJS.onCommand = async ( user, command, message, flags ) => {
 }
 ComfyJS.Init( process.env.TWITCHUSER, process.env.OAUTH );
 
+const ComfyDiscord = require( "comfydiscord" );
+ComfyDiscord.onCommand = async ( channel, user, command, message, flags, extra ) => {
+	if( command === "bday" ) {
+      if( message ) {
+        // figure out month/day/username
+        try {
+          var parts = message.split( " " );
+          var month = parts[ 0 ].toLowerCase();
+          var day = parts[ 1 ];
+          var username = parts[ 2 ] || user;
+          var months = [ "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec" ];
+          var monthNum = months.indexOf( month.substring( 0, 3 ) );
+          var userData = store.get( "birthdays" ) || {};
+          if( parts[ 2 ] && !flags.broadcaster && !flags.mod ) {
+            return;
+          }
+          if( month && day && username ) {
+            var dayNum = parseInt( day );
+            if( monthNum >= 0 ) {
+              userData[ username.toLowerCase() ] = {
+                name: username,
+                month: monthNum,
+                day: dayNum
+              };
+              store.put( "birthdays", userData );
+              ComfyDiscord.Say( channel, "Saved Birthday: " + monthToString(monthNum) + " " + dayNum );
+            }
+          }
+          else {
+            if( monthNum >= 0 ) {
+              // Print out the birthdays of the month
+              var birthdayPeeps = getBirthdays( monthNum );
+              ComfyDiscord.Say( channel, monthToString(monthNum) + " birthdays are: " + birthdayPeeps.map( x => `${x.name} (${monthToString(x.month)} ${x.day})` ).join( ", " ) );
+            }
+            else {
+              // Check it as a username
+              if( userData[ month ] ) {
+                var birthday = userData[ month ];
+                console.log( birthday );
+                ComfyDiscord.Say( channel, birthday.name + "'s birthday is: " + monthToString( birthday.month ) + " " + birthday.day );
+              }
+            }
+          }
+        }
+        catch( err ) {
+          console.error( err );
+        }
+      }
+      else {
+        // if( flags.broadcaster || flags.mod ) {
+          // Print out the birthdays of the month
+          var birthdayPeeps = getBirthdays( new Date().getMonth() );
+          ComfyDiscord.Say( channel, "This month's birthdays are: " + birthdayPeeps.map( x => `${x.name} (${monthToString(x.month)} ${x.day})` ).join( ", " ) );
+        // }
+      }
+    }
+    else if( command === "horoscope" ) {
+      var userData = store.get( "birthdays" ) || {};
+      var username = user.toLowerCase();
+      if( message ) {
+        // Check it as a username
+        username = message.replace(/[^a-zA-Z0-9_]/g, "").toLowerCase();
+      }
+
+      if( userData[ username ] ) {
+        var birthday = userData[ username ];
+        console.log( birthday );
+        let zodiac = await fetch( `http://localhost:1111/zodiac?month=${birthday.month + 1}&date=${birthday.day}` ).then( r => r.json() );
+        if( zodiac.sign ) {
+          let horoscope = await fetch( `http://localhost:1111/horoscope?fast=true&user=${username}&sign=${zodiac.sign}&noTwitch=true` ).then( r => r.json() );
+	ComfyDiscord.Say( channel, `${username} - ${zodiac.sign}: ${horoscope.text}` );
+        }
+      }
+      else {
+        // Show username not found
+        ComfyDiscord.Say( channel, `${username} was not found. Type !bday [month] [date] to add your birthday!` );
+      }
+    }
+};
+ComfyDiscord.Init( process.env.DISCORDTOKEN );
+
 function monthToString( monthNum ) {
   var months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
   return months[ monthNum ];
